9cfec0ce6e0e2a31310460cde101f7d1f8a5805f
Jaymart Araga <jaymart@thethumbfactory.com.au>
2020-06-21 19:24:20 +0800

    Integrate AppbarConfiguration

--- app/src/main/java/com/example/shopmart/ui/MainActivity.kt
+++ app/src/main/java/com/example/shopmart/ui/MainActivity.kt
@@ -1,8 +1,32 @@
 package com.example.shopmart.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.shopmart.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setSupportActionBar(toolbarMain)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        toolbarMain.setupWithNavController(navController, appBarConfiguration)
+    }
+}
